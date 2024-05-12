88aa9ebcf31f2b4d3281e5c666bfff6a4dd7419b
EPhill98 <euan.98@live.co.uk>
2024-05-12 22:50:39 +0800

    added photos to homepage

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,11 +1,11 @@
 const imgArry = ["media/photos/fujiPic.JPG",
                 "media/photos/hkhike.jpg",
                 "media/photos/hkSunset.jpeg",
-                "media/photos/volleyball.jpeg"]
+                ]
 var count = 0;
 
 function changeImage() {
-    document.getElementById("homeImg").src = imgArry[count%4];
+    document.getElementById("homeImg").src = imgArry[count%3];
     count++;
 }
 

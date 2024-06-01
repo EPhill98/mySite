4369fc518bf3fd46e5fd5b28d636177412b650db
EPhill98 <euan.98@live.co.uk>
2024-06-01 12:08:53 +0100

    added responsive imgs to sportsHobbie

--- javascript/sports.js
+++ javascript/sports.js
@@ -1,28 +1,39 @@
 class SportHobbie {
-    constructor(name, desc, date) {
+    constructor(name, desc, img1, img2) {
         this.name = name;
         this.desc = desc;
-        this.date = date;
+        this.img1 = img1
+        this.img2 = img2
     }
 }
 
 s1 = new SportHobbie("Volleyball",
-                    `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi exercitationem ducimus neque maiores asperiores
+    `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi exercitationem ducimus neque maiores asperiores
                         culpa ratione facere magni, repudiandae vel, dolores ab, dolorem aliquid dignissimos nobis ea perferendis
                         laboriosam illum!`,
-                    "7 Years");
+    "/Users/euanphillips/Desktop/mySite/media/photos/volleyball.jpeg",
+    "/Users/euanphillips/Desktop/mySite/media/photos/volleyball.jpeg");
 s2 = new SportHobbie("Bouldering",
-                    `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi exercitationem ducimus neque maiores asperiores
+    `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi exercitationem ducimus neque maiores asperiores
                         culpa ratione facere magni, repudiandae vel, dolores ab, dolorem aliquid dignissimos nobis ea perferendis
                         laboriosam illum!`,
-                    "1 Year");
+    "../media/photos/hkhike.jpg",
+    "../media/photos/hkhike.jpg");
+
+s3 = new SportHobbie("Hiking + Outdoor",
+    `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi exercitationem ducimus neque maiores asperiores
+                        culpa ratione facere magni, repudiandae vel, dolores ab, dolorem aliquid dignissimos nobis ea perferendis
+                        laboriosam illum!`,
+    "../media/photos/jzpp.jpg",
+    "../media/photos/hkhike.jpg");
 
 
-lst = [s1, s2];
+lst = [s1, s2, s3];
 
 
 function pressMe(x) {
     document.getElementById("cardJSH").innerHTML = lst[x].name;
     document.getElementById("cardJSB").innerHTML = lst[x].desc;
-    document.getElementById("cardJSF").innerHTML = lst[x].date;
+    document.getElementById("hobbieImg1").src = lst[x].img1;
+    document.getElementById("hobbieImg2").src = lst[x].img2;
 }

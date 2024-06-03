e562b9c3ad01c3661d949fbeab3a02907cb7155f
EPhill98 <euan.98@live.co.uk>
2024-06-03 12:03:31 +0100

    Added content to sportsHobbies, changed icons to bootstrap icons

--- javascript/sports.js
+++ javascript/sports.js
@@ -8,22 +8,41 @@ class SportHobbie {
 }
 
 s1 = new SportHobbie("Volleyball",
-    `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi exercitationem ducimus neque maiores asperiores
-                        culpa ratione facere magni, repudiandae vel, dolores ab, dolorem aliquid dignissimos nobis ea perferendis
-                        laboriosam illum!`,
+    `I have been playing volleyball for around 8 years after joining my local team in Stafford. 
+        Ever since, I have become hugely passionate about the sport. 
+        After a few years, I was offered the chance to become a <b>Qualified Referee</b> through a course paid for by my local club. 
+        Since then, I have refereed a number of games for my local club and the university. <br><br>
+
+        I joined the Swansea University team in my first year and made the first team. 
+        I was then made <b>captain</b> at the start of my second year. 
+        This was a great season for our team, winning the division and promotion, and I was honored to lead a great squad. 
+        Furthermore, I was elected as the <b>Club Development Officer</b>, where I organized a weekly intramural league focusing on getting new players interested in the sport. <br><br>
+    
+        During my year abroad, I joined a social club hosted by CityU Hong Kong and met some great friends.
+        I am happy to return to Swansea's club as the <b>Treasurer</b> for the 24/25 season!`,
     "../media/photos/volleyball.jpeg",
     "../media/photos/v1.JPG");
 s2 = new SportHobbie("Bouldering",
-    `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi exercitationem ducimus neque maiores asperiores
-                        culpa ratione facere magni, repudiandae vel, dolores ab, dolorem aliquid dignissimos nobis ea perferendis
-                        laboriosam illum!`,
+    `More recently, I have become a climber! After having some experience with rock climbing when I was younger 
+        and joining the club at my high school for only £1 per session, 
+        I rediscovered my passion for solving small, physical challenges. <br><br>
+
+        I made use of a great student deal during my time in Hong Kong 
+        and enjoyed bouldering during the quieter periods. 
+        This was a great hobby that I could do by myself or with friends to take a nice break 
+        from the busy city and punishing humidity.`,
     "../media/photos/b1.jpg",
     "../media/photos/b2.HEIC");
 
 s3 = new SportHobbie("Hiking + Outdoor",
-    `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi exercitationem ducimus neque maiores asperiores
-                        culpa ratione facere magni, repudiandae vel, dolores ab, dolorem aliquid dignissimos nobis ea perferendis
-                        laboriosam illum!`,
+    `Another hobby I rediscovered during my time in Hong Kong was hiking. 
+        Hiking proved to be a great way to meet other like-minded students, 
+        as well as explore my new home. I have been fortunate enough to see some amazing sights, 
+        such as reaching Lion Rock's peak just in time to watch a beautiful sunset. 
+        Additionally, a friend and I set out to complete the MacLehose Trail,
+        a 100km long trail that stretches along the region.<br><br>
+
+        I also got the opportunity to enjoy paddle boarding along Sai Kung's beautiful coast and kayaking. `,
     "../media/photos/jzpp.jpg",
     "../media/photos/hkhike.jpg");
 

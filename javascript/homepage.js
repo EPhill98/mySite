2e6bdbd1cfe29cf265c59fd3a3c236ee50de82ed
EPhill98 <euan.98@live.co.uk>
2024-07-21 01:13:52 +0100

    added education info

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,11 +1,12 @@
-const imgArry = ["media/photos/fujiPic.JPG",
+const imgArry = [
                 "media/photos/hkhike.jpg",
-                "media/photos/hkSunset.jpeg"
+                "media/photos/hkSunset.jpeg",
+                "media/photos/cityU.HEIC"
                 ]
 var img = 1;
 
 function changeImage() {
-    document.getElementById("homeImg").src = imgArry[img%3];
+    document.getElementById("homeImg").src = imgArry[img%(imgArry.length)];
     img++;
 }
 
